75b97c40128f2086d511cb8c391f2be6ab95819d
Миша Чукарев <mishakrut11@gmail.com>
2023-03-09 00:09:03 +0400

    test3 complete

--- nist3.py
+++ nist3.py
@@ -1,19 +1,41 @@
-from math import sqrt
-from scipy.special import erfc
+import math
+import scipy
+from scipy.special import gammaincc
 
 
 def main():
-    bits = 1
-    # вычисляем количество единиц и нулей в последовательности
+    bits = '00000000000000000000000000000000000000000000000000000000000000000000000111000010011010110000111110010111000100100110100011100011'
 
-
-    P = erfc(S / sqrt(2))
-    # проверяем гипотезу на уровне значимости 0.01
-    if P < 0.01:
-        return "Failed  test"
-    else:
-        return "Passed  test"
-
-
+    num_blocks = 128 // 8
+    mas_count = []
+    for i in range(16):
+        block = bits[i * 8: (i + 1) * 8]
+        one = 0
+        max_one_in_block = 0
+        max_run = 0
+        for bit in block:
+            if bit == '1':
+                one += 1
+                max_one_in_block = max(max_one_in_block, one)
+            else:
+                one = 0
+        max_run = max(max_run, max_one_in_block)
+        mas_count.append(max_run)
+    print(mas_count)
+    v1 = sum(x <= 1 for x in mas_count)
+    v2 = sum(x == 2 for x in mas_count)
+    v3 = sum(x == 3 for x in mas_count)
+    v4 = sum(x > 4 for x in mas_count)
+    k0 = 0.2148
+    k1 = 0.3672
+    k2 = 0.2305
+    k3 = 0.1875
+    X = ((v1-16*k0)**2)/(16*k0)+((v2-16*k1)**2)/(16*k1) + \
+        ((v3-16*k2)**2)/(16*k2)+((v4-16*k3)**2)/(16*k3)
+    P=gammaincc(3/2,X/2)
+    print(P)
+    #0.0005039782007990067
+    
+    
 if __name__ == "__main__":
     main()
